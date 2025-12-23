33cea90a0cc8a07fd2d923bf051fcc777ee38c36
HaydenA0 <aregrag2005@gmail.com>
2025-12-23 19:25:17 +0100

    news

--- src/main.go
+++ src/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"strconv"
+)
+
+func inputInt(prompt string) int {
+	var buffer string
+	fmt.Printf("%s", prompt)
+	fmt.Scanln(&buffer)
+	o, err := strconv.Atoi(buffer)
+	if err != nil {
+		fmt.Print("Error converting, enter an integer\n")
+	}
+	return o
+
+}
+
 func main() {
-	print("Hello world\n")
+
+	pnumArgs := flag.Int("na", 2, "Number of scales to compare")
+	flag.Parse()
+	var scales []int
+	for i := 0; i < *pnumArgs; i++ {
+		prompt := fmt.Sprintf("Enter the %dth scale : > ", i)
+		scale := inputInt(prompt)
+		scales = append(scales, scale)
+	}
+
 }
