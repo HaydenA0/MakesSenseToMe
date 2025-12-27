bb4491183c17834944a19ed4d656d4bc11fe6978
HaydenA0 <aregrag2005@gmail.com>
2025-12-27 07:24:20 +0100

    refactor units and scaling: generalize for time and distance, switch to float64, add larger units

--- src/main.go
+++ src/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Unit struct {
-	name           string
-	valueInSeconds int
-	limit          int
+	name        string
+	valueInBase float64
+	limit       int
 }
 
 var timeUnits = []Unit{
@@ -20,7 +20,29 @@ var timeUnits = []Unit{
 	{"week", 604800, 4},
 	{"month", 2592000, 12},
 	{"year", 31536000, 100},
+	{"decade", 315360000, 10},
 	{"century", 3153600000, 10},
+	{"millennium", 31536000000, 10},
+	{"geological_era", 3153600000000, 10},
+	{"age_of_earth", 1.38e17, 10},
+	{"age_of_universe", 4.35e17, 10},
+}
+
+var distanceUnits = []Unit{
+	{"millimeter", 1, 10},
+	{"centimeter", 10, 100},
+	{"meter", 1000, 1000},
+	{"football_field", 100000, 100},
+	{"kilometer", 1000000, 1000},
+	{"ten_kilometer_city", 10000000, 10},
+	{"hundred_kilometer_country", 100000000, 10},
+	{"half_the_globe", 10800000000, 10},
+	{"spain_to_argentina", 11000000000, 10},
+	{"london_to_sydney", 17000000000, 10},
+	{"earth_to_moon", 384400000000, 100},
+	{"earth_to_sun", 149600000000000, 100},
+	{"earth_to_mars", 225000000000000, 100},
+	{"width_solar_system", 5900000000000000, 10},
 }
 
 func inputInt(prompt string) int {
@@ -35,22 +57,25 @@ func inputInt(prompt string) int {
 
 }
 
-func prettyPrint(scales_index int, scaleSeconds float32, unit Unit) {
+func prettyPrint(scales_index int, scaleSeconds float64, unit Unit) {
 	fmt.Println("+------------------+----------------------------+")
 	fmt.Printf("| %-16s | %-26v |\n", "OriginalValue", scales_index)
-	fmt.Printf("| %-16s | %-26.2f |\n", "ScaleValue", scaleSeconds/float32(unit.valueInSeconds))
+	fmt.Printf("| %-16s | %-26.2f |\n", "ScaleValue", scaleSeconds/float64(unit.valueInBase))
 	fmt.Printf("| %-16s | %-26s |\n", "UnitName", unit.name)
 	fmt.Println("+------------------+----------------------------+")
 }
-func handleTimeScale(scales []int) {
-	normalizedScale := make([]float32, len(scales))
+func normalizeScale(scales []int) []float64 {
+	normalizedScale := make([]float64, len(scales))
 	for index := range scales {
-		normalizedScale[index] = float32(scales[index]) / float32(scales[0])
+		normalizedScale[index] = float64(scales[index]) / float64(scales[0])
 	}
-	fmt.Println("DEBUG : normalizedScale : ", normalizedScale)
+	return normalizedScale
+}
+func handleScales(scales []int, unitSlice []Unit) {
+	normalizedScale := normalizeScale(scales)
 	for index, scaleSeconds := range normalizedScale {
-		for _, unit := range timeUnits {
-			if scaleSeconds <= float32(unit.limit)*float32(unit.valueInSeconds) {
+		for _, unit := range unitSlice {
+			if scaleSeconds <= float64(unit.limit)*float64(unit.valueInBase) {
 				prettyPrint(scales[index], scaleSeconds, unit)
 				break
 			}
@@ -70,9 +95,9 @@ func main() {
 	}
 	switch *pScaleType {
 	case "time":
-		handleTimeScale(scales[:])
+		handleScales(scales[:], timeUnits[:])
 
 	case "distance":
-		fmt.Printf("You chose %s ?\n", *pScaleType)
+		handleScales(scales[:], distanceUnits[:])
 	}
 }
